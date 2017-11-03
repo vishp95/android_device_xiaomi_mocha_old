c4c12e07c197d7b5a0a7f747ac948063f735a62b
arttttt <artem-bambalov@yandex.ru>
2017-11-04 02:43:57 +0300

    mocha: we are using common nvidia vendor

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 
-$(call inherit-product-if-exists, vendor/xiaomi/mocha/shield/mocha.mk)
+$(call inherit-product-if-exists, vendor/nvidia/shield/mocha.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
