a7084d9b1b57a04d70ca466f3a2dc01ff16556de
arttttt <artem-bambalov@yandex.ru>
2018-01-13 13:16:29 +0300

    mocha: conn_init 2.0 * fixed problem with bt mac address
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- conn_init/conn_init.c
+++ conn_init/conn_init.c
@@ -21,25 +21,41 @@
 #include <cutils/properties.h>
 #include <errno.h>
 
+#define TAG "conn_init"
 #define MAC_PARTION "/dev/block/platform/700b0600.sdhci/by-name/BKB"
 #define MAC_PARTION_OLD "/dev/block/platform/sdhci-tegra.3/by-name/BKB"
-#define BT_MAC_PROP "persist.service.bdroid.bdaddr"
+#define BT_MAC_PROP "ro.bt.bdaddr_path"
 #define WIFI_MAC_PROP "/sys/module/bcmdhd/parameters/mac"
+#define BT_MAC_FILE "/data/misc/bluetooth/bt_mac.conf"
 
-void set_macs(FILE *fp)
+void set_bt_mac(FILE *fp) {
+	char buf[30];
+	FILE *bmfp;
+
+	fseek(fp, 0, SEEK_SET);
+	fread(buf, sizeof(char), 22, fp);
+
+	bmfp = fopen(BT_MAC_FILE, "w");
+	if (bmfp == NULL) {
+		ALOGE("%s: Can't open %s error: %d", TAG, BT_MAC_FILE, errno);
+		return;
+	} else {
+		fprintf(bmfp, "%02x:%02x:%02x:%02x:%02x:%02x\n", 
+			(unsigned char)buf[14],
+			(unsigned char)buf[13],
+			(unsigned char)buf[12],
+			(unsigned char)buf[11],
+			(unsigned char)buf[10],
+			(unsigned char)buf[9]);
+		fclose(bmfp);
+	}
+	property_set(BT_MAC_PROP, BT_MAC_FILE);
+}
+
+void set_wifi_mac(FILE *fp)
 {
 	char buf[30];
-	fread(buf, sizeof(char), 22, fp);
 	
-	snprintf(buf, 30, "%x:%x:%x:%x:%x:%x\n",
-		(unsigned char)buf[14],
-		(unsigned char)buf[13],
-		(unsigned char)buf[12],
-		(unsigned char)buf[11],
-		(unsigned char)buf[10],
-		(unsigned char)buf[9]);
-	property_set(BT_MAC_PROP, buf);
-
 	fseek(fp, sizeof(char) * 22, SEEK_SET);
 	fread(buf, sizeof(char), 22, fp);
 
@@ -53,7 +69,7 @@ void set_macs(FILE *fp)
 
 	FILE *wfp = fopen(WIFI_MAC_PROP, "w");
 	if (wfp == NULL) {
-		ALOGE("Can't open /sys/module/bcmdhd/parameters/mac error: %d", errno);
+		ALOGE("%s: Can't open %s error: %d", TAG, WIFI_MAC_PROP, errno);
 		return;
 	}
 	fwrite(buf, sizeof(unsigned char), 30, wfp);
@@ -65,14 +81,15 @@ int main(void)
 	FILE *fp;
 	fp = fopen(MAC_PARTION, "r");
 	if (fp == NULL) {
-		ALOGE("Can't open /dev/block/platform/700b0600.sdhci/by-name/BKB error: %d", errno);
+		ALOGE("%s: Can't open %s error: %d", TAG, MAC_PARTION, errno);
 		fp = fopen(MAC_PARTION_OLD, "r");
 		if (fp == NULL) {
-			ALOGE("Can't open /dev/block/platform/sdhci-tegra.3/by-name/BKB error: %d", errno);
+			ALOGE("%s: Can't open %s error: %d", TAG, MAC_PARTION_OLD, errno);
 			return 0;
 		}
 	}
-	set_macs(fp);
+	set_wifi_mac(fp);
+	set_bt_mac(fp);
 	fclose(fp);
 
 	return 0;
