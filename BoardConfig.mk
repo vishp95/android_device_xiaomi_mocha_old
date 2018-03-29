6932624d0f9e1527a4edc43b5dd4df54996b37b4
arttttt <artem-bambalov@yandex.ru>
2018-03-29 11:59:39 +0300

    mocha: use separated kernel device tree
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mocha
 TARGET_KERNEL_CONFIG := tegra12_android_defconfig
 BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/mocha/mkbootimg.mk
 
