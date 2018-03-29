6d84d2fd9de446ae4156c2b3ba8b5b3b1cc99c4a
arttttt <artem-bambalov@yandex.ru>
2018-03-29 17:56:30 +0300

    mocha: binderize health HAL
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- hidl.mk
+++ hidl.mk
@@ -26,7 +26,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
