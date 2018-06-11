72848b568513509a5d7a0fca70aaa81aeba8307d
arttttt <artem-bambalov@yandex.ru>
2018-06-11 23:11:27 +0300

    mocha: correct name for tinymix module
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- tinyalsa/Android.mk
+++ tinyalsa/Android.mk
@@ -29,7 +29,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= device/xiaomi/mocha/tinyalsa/include
 LOCAL_SRC_FILES:= tinymix.c
-LOCAL_MODULE := utils/tinymix_mocha
+LOCAL_MODULE := tinymix_mocha
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa_mocha
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Werror
